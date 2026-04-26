3f5582b76e2fb41ce010748fce43767bdebc308f
Dashulous <Dashulous@DESKTOP-0E0TM9E>
2026-04-26 18:29:03 +0200

    Added new data types for Muzzle and Barrel and added basic assets

---
+++ Assets/Scripts/Interchangable Parts/SO/PartsData/BarrelData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gun/Parts/Barrel")]
+public class BarrelData : PartData
+{
+    public string name;
+    public GameObject barrelObject;
+}
--- Assets/Scripts/Interchangable Parts/SO/PartsData/MagazineData.cs
+++ Assets/Scripts/Interchangable Parts/SO/PartsData/MagazineData.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Gun/Parts/Magazine")]
 public class MagazineData : PartData
 {
+    public string name;
+    public GameObject magazineObject;
     public int magSize;
 }
